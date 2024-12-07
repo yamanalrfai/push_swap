f083d27e2936c690a777fbd87e8041a5725c741d
Yaman Alrfai <yalrfai@c2r3s7.42amman.com>
2024-12-07 20:53:53 +0300

    100%

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yalrfai <yalrfai@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/21 15:45:35 by yaman-alrif       #+#    #+#             */
-/*   Updated: 2024/12/07 20:10:21 by yalrfai          ###   ########.fr       */
+/*   Updated: 2024/12/07 20:52:26 by yalrfai          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,22 +35,25 @@ void	sort_algo(t_node **s_a, t_node **s_b, t_cheepest_move *tmp, int size)
 		push(s_a, s_b, "pb\n");
 		size -= 2;
 	}
-	while (size-- > 2)
+	if (size == 3)
+		sort_three (s_a);
+	else
 	{
-		tmp->the_cost = -1;
-		sort_a_b(s_a, s_b, tmp);
-		tmp_doit(s_a, s_b, tmp);
-		push(s_a, s_b, "pb\n");
+		while (size-- > 2)
+		{
+			tmp->the_cost = -1;
+			sort_a_b(s_a, s_b, tmp);
+			tmp_doit(s_a, s_b, tmp);
+			push(s_a, s_b, "pb\n");
+		}
 	}
-	size = size_node(*s_b);
-	while (size-- != 0)
+	while (size_node(*s_b) != 0)
 	{
 		tmp->the_cost = -1;
 		sort_b_a(s_a, s_b, tmp);
 		tmp_doit(s_a, s_b, tmp);
 		push(s_b, s_a, "pa\n");
 	}
-	make_it_okay(s_a);
 }
 
 void	make_it_okay(t_node **stack_a)
@@ -103,7 +106,7 @@ int	main(int c, char **v)
 		return (1);
 	}
 	sort_algo(&stack_a, &stack_b, tmp, size_node(stack_a));
+	make_it_okay(&stack_a);
 	ft_fre(&stack_a, &stack_b, tmp);
 	return (0);
 }
-/*➜  push_swap ./a.out -4127 1 -396 3700 -107 -2882 -844 -2967 1463 -4199 -2947 -3699 -3041 -2996 4699 1555 -3614 -1454 -3831 -180 -1319 -1278 -3345 3655 -3804 -3168 325 1932 3257 -2732 1433 1534 476 1494 1641 -4311 1887 3233 -1072 4143 -199 -4505 3314 809 -2770 4937 2213 -138 1259 2872 1770 333 3122 2031 4370 4241 19 -3368 3725 -247 -2145 3525 -3360 -615 3584 2664 -419 -2682 3945 4886 442 2123 -4825 -238 -2028 2092 -2754 2057 1163 1916 -4545 -4623 916 -1865 -2217 -3585 2072 -769 -3213 -2734 1351 793 -1342 2663 -1562 -3503 3610 3805 395 469 */
