a855e452d5733142fdef9c3e1db8767e57895e02
Yaman Alrfai <yalrfai@c2r2s10.42amman.com>
2024-12-16 20:34:32 +0300

    nice

--- libft/libft.h
+++ libft/libft.h
@@ -23,15 +23,6 @@
 # define FTINTMAX 2147483647
 # define FTINTMIN -2147483648
 
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}					t_list;
-
-t_list				*ft_lstlast(t_list *lst);
-t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
-						void (*del)(void *));
 int					ft_isalpha(int c);
 int					ft_isdigit(int c);
 int					ft_isalnum(int c);
@@ -43,7 +34,6 @@ int					ft_getlen(int size, char **strs, char *sep);
 int					ft_strncmp(const char *s1, const char *s2, size_t n);
 int					ft_memcmp(const void *s1, const void *s2, size_t n);
 long int			ft_atoi(const char *nptr);
-int					ft_lstsize(t_list *lst);
 char				*ft_strchr(const char *s, int c);
 char				*ft_strrchr(const char *s, int c);
 char				*ft_strnstr(const char *big, const char *lit, size_t len);
@@ -60,12 +50,6 @@ int					ft_putstr_fd(char *s, int fd);
 void				ft_putendl_fd(char *s, int fd);
 int					ft_putnbr_fd(long long nb, int fd, int base);
 void				ft_striteri(char *s, void (*f)(unsigned int, char *));
-void				ft_lstadd_front(t_list **lst, t_list *new);
-void				ft_lstadd_back(t_list **lst, t_list *new);
-void				ft_lstdelone(t_list *lst, void (*del)(void *));
-void				ft_lstclear(t_list **lst, void (*del)(void *));
-void				ft_lstiter(t_list *lst, void (*f)(void *));
-void				*ft_memset(void *s, int c, int n);
 void				*ft_memcpy(void *dest, const void *src, size_t n);
 void				*ft_memmove(void *dest, const void *src, size_t n);
 void				*ft_memchr(const void *s, int c, size_t n);
--- makefile
+++ makefile
@@ -4,7 +4,8 @@ LIBDIR = libft
 LIB = $(LIBDIR)/libft.a
 
 OBJS = $(SRC:.c=.o)
-SRC = node_comande.c soet_algo.c main.c free_file.c cheek_in.c choose_sort.c tar_find.c tmp_thing.c until_stack.c num_cheek.c
+SRC =	node_comande.c soet_algo.c main.c free_file.c cheek_in.c choose_sort.c tar_find.c tmp_thing.c until_stack.c num_cheek.c \
+		libft/ft_getlen.c libft/ft_putnbr_fd_lower.c libft/ft_putadd_fd.c libft/ft_printf.c libft/ft_putnbr_fd.c libft/ft_putendl_fd.c libft/ft_putchar_fd.c libft/ft_putstr_fd.c libft/ft_striteri.c libft/ft_strmapi.c libft/ft_itoa.c libft/ft_split.c libft/ft_strtrim.c libft/ft_strjoin.c libft/ft_substr.c libft/ft_strdup.c libft/ft_calloc.c libft/ft_atoi.c libft/ft_strnstr.c libft/ft_memcmp.c libft/ft_memchr.c libft/ft_strncmp.c libft/ft_strrchr.c libft/ft_strchr.c libft/ft_toupper.c libft/ft_tolower.c libft/ft_strlcpy.c libft/ft_strlcat.c libft/ft_memmove.c libft/ft_memcpy.c libft/ft_bzero.c libft/ft_memset.c libft/ft_strlen.c libft/ft_isprint.c libft/ft_isascii.c libft/ft_isalnum.c libft/ft_isdigit.c libft/ft_isalpha.c
 NAME = push_swap
 
 all: $(NAME)
